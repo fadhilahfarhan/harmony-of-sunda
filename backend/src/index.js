532a83086d69bfdec4c6b7a811bc9529e2c1cdd0
FadhilahFarhan <fadhilahfarhan2004@gmail.com>
2023-11-19 01:21:16 +0700

    Added port configuration from environment variable to index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,13 @@ import cors from 'cors';
 import router from './router/router.js';
 
 const app = express();
+const port = process.env.APP_PORT
 
 app.use(cors())
 app.use(urlencoded({extended: false}));
 app.use(json())
 app.use(router)
 
-app.listen(3000, ()=>{
-  console.log('Status: Running, Port: 3000');
+app.listen(port, () => {
+  console.log(`Status: Running, Port: ${port}`);
 })
